ca42b9bd7f1ee1be277fefc2aa2b897863c842d5
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2023-06-22 00:46:43 +0100

    Created the contoller

---
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.employee.controller;
+
+import com.example.employee.entity.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @GetMapping("/showEemployees")
+    public ModelAndView showEployee(){
+        ModelAndView mav = new ModelAndView("list-employees");
+        List<Employee> list = employeeRepository.findAll();
+        mav.addObject("employess", list);
+        return mav;
+    }
+
+}
